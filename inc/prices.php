8d2f911527316c161fcce9e08fa7c3e309c2a2ef
Dexter84 MAC <fenixzaec84@gmail.com>
2026-05-28 15:59:26 +0700

    add prices

--- inc/prices.php
+++ inc/prices.php
@@ -1,5 +1,12 @@
-<section class="prices df fdc aife">
-	<div class="container df fdc aic">
-		<div class="services-list"></div>
-	</div>
-</section>
+	<section class="prices df fdc aife">
+		<div class="container df fdc aic">
+			<p class="projects-list-title">Прейскурант работ:</p>
+			<div class="services-list df fdc">
+				<div class="row df aifs jcsb">
+					<p class="name"><b>Услуга:</b></p>
+					<p class="code"><b>Код:</b></p>
+					<p class="price"><b>Стоимость вкл. НДС за 1 час:</b></p>
+				</div>
+			</div>
+		</div>
+	</section>
